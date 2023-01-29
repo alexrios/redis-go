0316f63e0f08ef081b325e58c01e21ae9b322346
Alex Rios <alex.rios@pm.me>
2023-01-29 01:53:03 -0300

    pong

--- app/server.go
+++ app/server.go
@@ -17,9 +17,22 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	// store incoming data
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+		return
+	}
+	// respond
+	conn.Write([]byte("+PONG"))
+	// close conn
+	conn.Close()
 }
