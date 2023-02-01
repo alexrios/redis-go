1db780adfa19c59bc1dad5ae08b09de1aa4d6184
Alex Rios <alex.rios@pm.me>
2023-02-01 00:33:07 -0300

    multi client

--- app/server.go
+++ app/server.go
@@ -6,36 +6,85 @@ import (
 	"os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+type Reponse struct {
+	msg  []byte
+	conn net.Conn
+}
 
-	// Uncomment this block to pass the first stage
-	//
+func (r Reponse) StrMsg() string {
+	return string(r.msg)
+}
+
+func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	// close conn
-	defer conn.Close()
-
-	// store incoming data
-	for {
-		buffer := make([]byte, 1024)
-		_, err = conn.Read(buffer)
-		if err != nil {
-			fmt.Println(err)
+	defer func(l net.Listener) {
+		if err := l.Close(); err != nil {
+			fmt.Println("Failed to close the tcp listener: %w", err)
 			os.Exit(1)
-			return
 		}
-		// respond
-		conn.Write([]byte("+PONG\r\n"))
-	}
+	}(l)
 
+	conns := make(chan net.Conn)
+	deadConns := make(chan net.Conn)
+	responses := make(chan Reponse)
+
+	// send to the connections channel.
+	go func() {
+		for {
+			var conn net.Conn
+			conn, err := l.Accept()
+			conns <- conn
+
+			if err != nil {
+				fmt.Println("Error accepting connection: ", err.Error())
+				os.Exit(1)
+			}
+		}
+	}()
+
+	// Iterate over all channels to decide what to do.
+	// THIS IS THE EVENT LOOP
+	for {
+		select {
+		// brand new connecion
+		case conn := <-conns:
+			// new routine for a new connection
+			go func() {
+				// store incoming data
+				buffer := make([]byte, 1024)
+				_, err = conn.Read(buffer)
+				if err != nil {
+					fmt.Println(err)
+					// notify the program that a connection is not available anymore.
+					deadConns <- conn
+					return
+				}
+				responses <- Reponse{
+					msg:  buffer,
+					conn: conn,
+				}
+			}()
+		case dc := <-deadConns:
+			_ = dc.Close()
+		case r := <-responses:
+			// handle protocol messages
+			var wError error
+			switch r.StrMsg() {
+			case "PING":
+				_, wError = r.conn.Write([]byte("+PONG\r\n"))
+			default:
+				_, wError = r.conn.Write([]byte("+WTF\r\n"))
+			}
+			if wError != nil {
+				fmt.Println(wError)
+				// notify the program that a connection is not available anymore.
+				deadConns <- r.conn
+			}
+
+		}
+	}
 }
